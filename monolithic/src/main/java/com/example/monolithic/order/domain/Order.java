70b1626b5fb16a776174898da112c4c7af2463f7
휘래 김 <hramst0618@gmail.com>
2026-02-16 19:25:31 +0900

    feat: 주문 로직 작성

---
+++ monolithic/src/main/java/com/example/monolithic/order/domain/Order.java
@@ -0,0 +1,16 @@
+package com.example.monolithic.order.domain;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+
+@Getter
+@Entity
+@Table(name = "orders")
+public class Order {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    public Order() {
+    }
+}
---
+++ monolithic/src/main/java/com/example/monolithic/order/infrastructure/OrderItemRepository.java
@@ -0,0 +1,8 @@
+package com.example.monolithic.order.infrastructure;
+
+import com.example.monolithic.order.domain.OrderItem;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface OrderItemRepository extends JpaRepository<OrderItem, Long> {
+
+}
