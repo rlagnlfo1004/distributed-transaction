214b723c5b0f28247b58ad9ba9f8057f6e78e6ca
휘래 김 <hramst0618@gmail.com>
2026-02-16 23:34:37 +0900

    refactor: Lock을 활용하여 주문 로직이 1번만 수행되도록 변경

--- monolithic/src/main/java/com/example/monolithic/order/controller/OrderController.java
+++ monolithic/src/main/java/com/example/monolithic/order/controller/OrderController.java
@@ -1,6 +1,7 @@
 package com.example.monolithic.order.controller;
 
 import com.example.monolithic.order.application.OrderService;
+import com.example.monolithic.order.application.RedisLockService;
 import com.example.monolithic.order.application.dto.CreateOrderResult;
 import com.example.monolithic.order.controller.dto.CreateOrderRequest;
 import com.example.monolithic.order.controller.dto.CreateOrderResponse;
@@ -15,6 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class OrderController {
 
     private final OrderService orderService;
+    private final RedisLockService redisLockService;
 
     @PostMapping("/order")
     public CreateOrderResponse createOrder(@RequestBody CreateOrderRequest request) {
@@ -24,6 +26,17 @@ public class OrderController {
 
     @PostMapping("/order/place")
     public void placeOrder(@RequestBody PlaceOrderRequest request) throws InterruptedException {
-        orderService.placeOrder(request.toPlaceOrderCommand());
+        String key = "order:monolithic:" + request.orderId();
+        boolean acquiredLock = redisLockService.tryLock(key, request.orderId().toString());
+
+        if (!acquiredLock) {
+            throw new RuntimeException("락 획득에 실패하였습니다.");
+        }
+
+        try{
+            orderService.placeOrder(request.toPlaceOrderCommand());
+        } finally {
+            redisLockService.releaseLock(key);
+        }
     }
 }
