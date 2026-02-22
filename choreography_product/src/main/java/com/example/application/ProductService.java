5e8dfa6fb6a3769de1cd5acd1dbe52a03e95c7e2
휘래 김 <hramst0618@gmail.com>
2026-02-23 00:01:28 +0900

    test: 포인트 차감 실패 및 롤백 시나리오

--- choreography_product/src/main/java/com/example/application/ProductService.java
+++ choreography_product/src/main/java/com/example/application/ProductService.java
@@ -104,10 +104,6 @@ public class ProductService {
             );
         }
 
-        if (true) {
-            throw new RuntimeException("강제 예외 발생");
-        }
-
         return new ProductBuyCancelResult(totalPrice);
     }
 }
